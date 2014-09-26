8763095ca62a082fd8ccaa2874bece6b509dad34
Sergio Miguel Delgado <alu0100706468@ull.edu.es>
2014-09-26 10:54:44 +0100

    Código optimizado

--- twitter.rb
+++ twitter.rb
@@ -4,28 +4,36 @@ require 'sinatra'
 require './configure'
 
 	get '/' do
-		@n_amigos = 0 # numero de amigos
-		@name = '' #nombre de usuario twitter
+		@n_amigos = 0 
+		@name = '' 
 		@pic = ''
-		@usuarios = Hash.new	#almacenara los amigos y el numero de seguidores
+		@usuarios = Hash.new
 		erb :twitter
 	end
 
 	post '/' do
 		@n_amigos = 0
 		@usuarios = Hash.new
-		@name = params[:firstname] || '' #recoge del parametro firstname el nombre de usuario
-		client = my_twitter_client() #establece conexion twitter
+		@name = params[:firstname] || ''
+		client = my_twitter_client()
 
 
 		#Si el usuario introducido es de Twitter:
 		if client.user? @name
 
-			usr = client.user(@name) #usr = usuario introducido por pantalla
-			@pic = usr.profile_image_url
-			@n_amigos = usr.friends_count #n_amigos = numero de amigos de usr
-			amigos = client.friend_ids(@name).attrs[:ids].take(10) #Almacena en "amigos" los últimos 10 amigos del usuario
+			#usr = usuario introducido por pantalla
+			usr = client.user(@name) 
 
+			#pic = url de la imagen de perfil
+			@pic = usr.profile_image_url
+			
+			#n_amigos = numero de amigos de usr
+			@n_amigos = usr.friends_count 
+			
+			#Almacena en "amigos" los últimos 10 amigos del usuario
+			amigos = client.friend_ids(@name).attrs[:ids].take(10) 
+
+			#Si tiene  menos de 10 amigos, el bucle será de n_amigos
 			if (@n_amigos < 10)
 				@n_amigos.times do |i|
 						user_n = client.user(amigos[i])
@@ -33,6 +41,8 @@ require './configure'
 				end
 			end
 
+
+			#Si tiene  más de o 10 amigos, el bucle será 10
 			if (@n_amigos >= 10)
 				10.times do |i|
 						user_n = client.user(amigos[i])
@@ -40,9 +50,8 @@ require './configure'
 				end
 			end
 
-			@usuarios = @usuarios.sort_by {|k,v| -v} #ordena los amigos de mayor a menor segun sus seguidores
+			@usuarios = @usuarios.sort_by {|user,followers| -followers} #ordena los amigos de mayor a menor segun sus seguidores
 		end
-
 		erb :twitter
 	end	
 
